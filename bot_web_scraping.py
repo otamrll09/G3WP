431f49b859701ca144124c156dc54a55a2423fdc
unknown <otaviomm97@gmail.com>
2022-05-13 15:23:16 -0300

    G3WS - Finalizado a mudança de pagina e padronizado a saida do bot.

--- bot_web_scraping.py
+++ bot_web_scraping.py
@@ -21,7 +21,7 @@ driver.get("https://nvd.nist.gov/vuln/search")
 
 #TODO: Na integração substituir as variaveis por valores de input
 data_busca = "03052022" #! A entrada de data deve estar no padrão americano mm dd YY
-sw_sch = "microsoft" 
+sw_sch = "log4J" 
 
 #TODO: Adicionar verificador do status do site para evitar crashs (com a biblioteca requests)
 try:
@@ -66,93 +66,118 @@ try:
     response = requests.get(url_n)
     content_t = response.content
     site_t = BeautifulSoup(content_t, 'html.parser')
+    num_vuln = site_t.find(attrs={"data-testid": "vuln-matching-records-count"})
+    num_vuln = int(str(num_vuln.contents)[2:-2])
+    print(type(num_vuln))
+    num_pg = []
+    lst_felps = []
+    if num_vuln > 20:
+        for busca in site_t.find_all(attrs={"aria-label": re.compile('Page')}):
+            num_pg.append("https://nvd.nist.gov"+str(busca["href"]))
+    else:
+        num_pg.append(url_n)
+    #print(num_pg)
 
-    #####
-    #* O for foi utilizado para localizar todos os <a> da HTML e a frente serão selecionados aqueles que possuem o termo "CVE"
-    for link in site_t.find_all('a'):
-        refadv = []
-        severity = []
-        affec =[]
-        src_lst = []
-        cpe_cd = []
-        publi_d = ''
-        if 'CVE' in str(link.contents):
-            #* Os links contidos na variavel localizada estavam incompletos, sendo necessario concatenar as str e gerar novo link.
-            new_link = "https://nvd.nist.gov" + str(link.get('href'))
+    for pg_b in num_pg:
+        response = requests.get(pg_b)
+        content_t = response.content
+        site_t = BeautifulSoup(content_t, 'html.parser')
+        #####
+        #* O for foi utilizado para localizar todos os <a> da HTML e a frente serão selecionados aqueles que possuem o termo "CVE"
+        for link in site_t.find_all('a'):
+            refadv = []
+            severity = []
+            affec =[]
+            src_lst = []
+            cpe_cd = []
+            publi_d = ''
+            sub_lst = []
+            if 'CVE' in str(link.contents):
+                #* Os links contidos na variavel localizada estavam incompletos, sendo necessario concatenar as str e gerar novo link.
+                new_link = "https://nvd.nist.gov" + str(link.get('href'))
 
-            driver.get(new_link)
-            cont_new = requests.get(new_link)
-            cont_new_t = cont_new.content
-            site_n_t = BeautifulSoup(cont_new_t, 'html.parser')
-            #print(site_n_t.prettify())
+                driver.get(new_link)
+                cont_new = requests.get(new_link)
+                cont_new_t = cont_new.content
+                site_n_t = BeautifulSoup(cont_new_t, 'html.parser')
+                #print(site_n_t.prettify())
 
-            #####
-            #* Utilizando método find do BS para localizar o código CVE, armazenado na variavel cve_cd.
-            cve_cd = site_n_t.find(attrs = {'data-testid':'page-header-vuln-id'})
-            cve_cd = str(cve_cd.contents)[1:-1] #* Retirado caracteres a mais.
-
-            #####
-            #* Mesmo modelo utilizado anterior mente agora para loclaizar a descrição.
-            descrit = site_n_t.find('p', attrs={'data-testid': 'vuln-description'})
-            descrit = str(descrit.contents)[1:-1]
-
-            #####
-            #* Utilizando o método "find_all" do BS para localizar os graus de severidade anotados da vunerabilidade.
-            for busca in site_n_t.find_all(attrs={'id': re.compile('Cvss3')}):
-                severity.append(str(busca.contents)[2:-2]) #? Verificar se é possivel identificar cada uma das notas
-            
-            #####
-            #* Mesmo principio porem agora para localizar links de esclarecimento do fornecedor do software
-            #* OBS.: Foi utilizado a biblioteca re junto com métodos compile para localizar de maneria geral palavras
-            #* que contenham a parte já especificada (inserido palavra parcial)
-            for busca in site_n_t.find_all(attrs= {'data-testid' : re.compile("vuln-hyperlinks-link-")}):
-                refadv.append(busca.text)
-            
-            #####
-            #* Iniciando com try pois existem casos em que não é especificado o código "CPE" e quando ocorria o sw bugava.
-            try:
                 #####
-                #* Localizado a linha do HTML com json(s) inserido(s).
-                for busca in site_n_t.find_all('input', attrs= {'id' : re.compile("cveTreeJsonDataHidden")}):
-                    tens = busca
+                #* Utilizando método find do BS para localizar o código CVE, armazenado na variavel cve_cd.
+                cve_cd = site_n_t.find(attrs = {'data-testid':'page-header-vuln-id'})
+                cve_cd = str(cve_cd.contents)[1:-1] #* Retirado caracteres a mais.
+
+                #####
+                #* Mesmo modelo utilizado anterior mente agora para loclaizar a descrição.
+                descrit = site_n_t.find('p', attrs={'data-testid': 'vuln-description'})
+                descrit = str(descrit.contents)[1:-1]
+
+                #####
+                #* Utilizando o método "find_all" do BS para localizar os graus de severidade anotados da vunerabilidade.
+                for busca in site_n_t.find_all(attrs={'id': re.compile('Cvss3')}):
+                    severity.append(str(busca.contents)[2:-2]) #? Verificar se é possivel identificar cada uma das notas
                 
                 #####
-                #* Retirado o(s) json da linha do HTML e feito o split para casos em que há mais de um json.
-                js_part = tens['value'][1:-1].split("[]}]}]},")
-
-                #####
-                #* Elaborado lista com os json corrigidos após o split.
-                for ptr in range(0,len(js_part)):
-                    if ptr == len(js_part)-1:
-                        src_lst.append(js_part[ptr])
-                    else:
-                        src_lst.append(js_part[ptr]+"[]}]}]}")
+                #* Mesmo principio porem agora para localizar links de esclarecimento do fornecedor do software
+                #* OBS.: Foi utilizado a biblioteca re junto com métodos compile para localizar de maneria geral palavras
+                #* que contenham a parte já especificada (inserido palavra parcial)
+                for busca in site_n_t.find_all(attrs= {'data-testid' : re.compile("vuln-hyperlinks-link-")}):
+                    refadv.append(busca.text)
                 
                 #####
-                #* Utilizado a biblioteca json + método loads para converter ele em um dicionario e ser tratado no codigo.
-                for rg in range(0,len(src_lst)):
-                    scr = json.loads(src_lst[rg])
-                    cpe_cd.append(scr["containers"][0]["containers"][0]["cpes"][0]["cpe23Uri"]) #? Seria possivel melhorar?
-                    #? Esse levantamento foi feito com base no padrão da loclaização da informação, mas está muito "truncado"                    
-            except:
-                cpe_cd.append("None")
+                #* Iniciando com try pois existem casos em que não é especificado o código "CPE" e quando ocorria o sw bugava.
+                try:
+                    #####
+                    #* Localizado a linha do HTML com json(s) inserido(s).
+                    for busca in site_n_t.find_all('input', attrs= {'id' : re.compile("cveTreeJsonDataHidden")}):
+                        tens = busca
+                    
+                    #####
+                    #* Retirado o(s) json da linha do HTML e feito o split para casos em que há mais de um json.
+                    js_part = tens['value'][1:-1].split("[]}]}]},")
 
-            #####
-            #* Mesma logica já utilizado para localizar conteudo com palavras parciais, agora para localizar a data de publicação.
-            for busca in site_n_t.find_all(attrs= {'data-testid' : re.compile("vuln-published-on")}):
-                publi_d = str(busca.contents)
+                    #####
+                    #* Elaborado lista com os json corrigidos após o split.
+                    for ptr in range(0,len(js_part)):
+                        if ptr == len(js_part)-1:
+                            src_lst.append(js_part[ptr])
+                        else:
+                            src_lst.append(js_part[ptr]+"[]}]}]}")
+                    
+                    #####
+                    #* Utilizado a biblioteca json + método loads para converter ele em um dicionario e ser tratado no codigo.
+                    for rg in range(0,len(src_lst)):
+                        scr = json.loads(src_lst[rg])
+                        cpe_cd.append(scr["containers"][0]["containers"][0]["cpes"][0]["cpe23Uri"]) #? Seria possivel melhorar?
+                        #? Esse levantamento foi feito com base no padrão da loclaização da informação, mas está muito "truncado"                    
+                except:
+                    cpe_cd.append("None")
 
-            print("Código CVE: ", cve_cd)
-            print("Descrição: ", descrit)
-            print("Severidade: ", severity)
-            print("Referencia: ", refadv)
-            print("Código CPE: ", cpe_cd)
-            print("Data de publicação: ", publi_d)
-            print("Site da CVE", new_link)
-            #time.sleep(1)
-            driver.back()
-    #####
-    #TODO: Pendente trabalhar na mudança de pagina da tabela com todos os CVE.
+                #####
+                #* Mesma logica já utilizado para localizar conteudo com palavras parciais, agora para localizar a data de publicação.
+                for busca in site_n_t.find_all(attrs= {'data-testid' : re.compile("vuln-published-on")}):
+                    publi_d = str(busca.contents)
+
+                sub_lst.append(sw_sch)
+                sub_lst.append(cve_cd)
+                #print("Código CVE: ", cve_cd)
+                sub_lst.append(descrit)
+                #print("Descrição: ", descrit)
+                sub_lst.append(severity)
+                #print("Severidade: ", severity)
+                sub_lst.append(refadv)
+                #print("Referencia: ", refadv)
+                sub_lst.append(cpe_cd)
+                #print("Código CPE: ", cpe_cd)
+                sub_lst.append(publi_d)
+                #print("Data de publicação: ", publi_d)
+                sub_lst.append(new_link)
+                #print("Site da CVE", new_link)
+                print(sub_lst)
+                lst_felps.append(sub_lst)
+                #time.sleep(1)
+                driver.back()
+        #####
     time.sleep(5)
 except:
     driver.quit()

42b7b0c9d4b9432c3f7cc0fb0c48d149130328c2
unknown <otaviomm97@gmail.com>
2022-05-16 15:37:38 -0300

    G3WS-16 - Ajuste nas funções do web scraping e adicionado entrada de varios email

--- bot_web_scraping.py
+++ bot_web_scraping.py
@@ -10,239 +10,179 @@ import requests
 import re
 import json
 
-from cgitb import text
-import pandas as pd
+#from cgitb import text
+#import pandas as pd
 # import numpy as np
-from openpyxl import load_workbook
-from openpyxl.styles import Alignment
-from openpyxl.styles import Font
+#from openpyxl import load_workbook
+#from openpyxl.styles import Alignment
+#from openpyxl.styles import Font
 # from openpyxl.styles import PatternFill
 
-import bot_registro_excel
+#import bot_registro_excel
 
 
-
-while True:
-    sw_sch = input("Insira o software que deseja pesquisar: ")#"log4J"
-    if sw_sch == '':
-        print('Dado invalido, digite novamente')
-        continue
-    else:
-        break
-
-#* Testador de datas SIMPLIFICADO, é necessario que as datas 
-while True:
-    while True:
-        data_busca = input("Insira a data de início do intervalo (formato mmddYY): ")#"01152022" #! A entrada de data deve estar no padrão americano mm dd YY
-        data_busca = data_busca.replace("/", "")
-        data_busca = data_busca.replace(" ", "")
-        if int(data_busca[0:2]) > 12:
-            print("Mes invalido")
-            continue
-        elif int(data_busca[2:4]) >= 32:
-            print("Dia invalido")
-            continue
-        elif (int(data_busca[0:2]) == 2) and (int(data_busca[2:4]) > 30):
-            print("Dia invalido")
-            continue
-        elif len(data_busca) != 6:
-            print("Formato de data invalido")
-            continue
-        break
-    while True:
-        aday = input("Insira a data de fim do intervalo (formato mmddYY): ")
-        aday = aday.replace("/", "")
-        aday = aday.replace(" ", "")
-        if int(aday[0:2]) > 12:
-            print("Mes invalido")
-            continue
-        elif int(aday[2:4]) >= 32:
-            print("Dia invalido")
-            continue
-        elif (int(aday[0:2]) == 2) and (int(aday[2:4]) > 30):
-            print("Dia invalido")
-            continue
-        elif len(aday) != 6:
-            print("Formato de data invalido")
-            continue
-        break
-
-    if int(data_busca[4:]) > int(aday[4:]):
-        print('Datas invalidas.')
-        continue
-    elif int(data_busca[2:4]) > int(aday[2:4]):
-        print('Datas invalidas.')
-        continue
-    elif (int(data_busca[2:4]) == int(aday[2:4])) and (int(data_busca[0:2]) > int(aday[0:2])):
-        print('Datas invalidas.')
-        continue
-    else:
-        break
-
-print("-------------------------------------------------------------------------------------")
-time.sleep(2)
-print("OK! Não se assuste vamos lá! Jájá chega o email em...")
-print("-------------------------------------------------------------------------------------")
-time.sleep(5)
-
-# * Apontamento do diretorio onde está o driver do Chrome (versão 101 dos navegadores).
-# * Logo mais é iniciado o Webdriver utilizando o driver especificado.
-PATH = 'C:\Program Files (x86)\chromedriver.exe'
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(PATH,chrome_options=options)
-today = date.today() # * Data de hoje.
-# * Site para coleta de dados
-driver.get("https://nvd.nist.gov/vuln/search")
-#TODO: Na integração substituir as variaveis por valores de input
-#TODO: Adicionar verificador do status do site para evitar crashs (com a biblioteca requests)
-try:
-    #* Função para buscar a localização do botão  que muda para modo "Advanced"
-    #* Foi utilizado funções da biblioteca Selenium Webdriver para localizar o botão.
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "SearchTypeAdvanced"))
-    )    
-    element.click() #* Método que clica automaticamente no local especificado na variavel "element".
-    #####
-    #* Localizando o campo de data inicial da busca.
-    #TODO: Insirir limitador para periodos de até 120 dias.
-    inp_date = driver.find_element(By.ID,"published-start-date")   
-    inp_date.send_keys(data_busca)  #* Método que insere a data no campo especificado.
-    #####
-    inp_date = driver.find_element(By.ID, "published-end-date")
-    # aday = today.strftime("%m%d%Y") #* Utilizando a biblioteca "datetime" para inserir a data atual do PC.
-    inp_date.send_keys(aday)
+def web_sc(sw_sch, data_busca, aday):
     
-    #####
-    #* Inserindo o software a ser pesquisado. 
-    inp_sw = driver.find_element(By.ID, "Keywords")
-    inp_sw.send_keys(sw_sch)
-    #####
-    #* Apertando o botão de "Search".
-    #? Seria bom tratar erros na busca? (Ex.: Periodo maior que 120 dias)
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "vuln-search-submit"))
-    )
-    element.click()
-    #####
-    #TODO: Verificar se a linha abaixo é necessaria.
-    url_n = driver.current_url
-    #####
-    #! A partir desse momento será utilizando muito mais o BeautifulSoup para tratar a HTML e o requests para acessar elas.
-    #* Usando o Requests para baixar a HTML e o Soup para "tratar" ela.
-    response = requests.get(url_n)
-    content_t = response.content
-    site_t = BeautifulSoup(content_t, 'html.parser')
-    num_vuln = site_t.find(attrs={"data-testid": "vuln-matching-records-count"})
-    num_vuln = int(str(num_vuln.contents)[2:-2])
-    #print(type(num_vuln))
-    num_pg = []
-    lst_felps = []
-    if num_vuln > 20:
-        for busca in site_t.find_all(attrs={"aria-label": re.compile('Page')}):
-            num_pg.append("https://nvd.nist.gov"+str(busca["href"]))
-    else:
-        num_pg.append(url_n)
-    #print(num_pg)
-    for pg_b in num_pg:
-        response = requests.get(pg_b)
+
+    # * Apontamento do diretorio onde está o driver do Chrome (versão 101 dos navegadores).
+    # * Logo mais é iniciado o Webdriver utilizando o driver especificado.
+    PATH = 'C:\Program Files (x86)\chromedriver.exe'
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(PATH,chrome_options=options)
+    today = date.today() # * Data de hoje.
+    # * Site para coleta de dados
+    driver.get("https://nvd.nist.gov/vuln/search")
+    #TODO: Na integração substituir as variaveis por valores de input
+    #TODO: Adicionar verificador do status do site para evitar crashs (com a biblioteca requests)
+    try:
+        #* Função para buscar a localização do botão  que muda para modo "Advanced"
+        #* Foi utilizado funções da biblioteca Selenium Webdriver para localizar o botão.
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "SearchTypeAdvanced"))
+        )    
+        element.click() #* Método que clica automaticamente no local especificado na variavel "element".
+        #####
+        #* Localizando o campo de data inicial da busca.
+        #TODO: Insirir limitador para periodos de até 120 dias.
+        inp_date = driver.find_element(By.ID,"published-start-date")   
+        inp_date.send_keys(data_busca)  #* Método que insere a data no campo especificado.
+        #####
+        inp_date = driver.find_element(By.ID, "published-end-date")
+        # aday = today.strftime("%m%d%Y") #* Utilizando a biblioteca "datetime" para inserir a data atual do PC.
+        inp_date.send_keys(aday)
+        
+        #####
+        #* Inserindo o software a ser pesquisado. 
+        inp_sw = driver.find_element(By.ID, "Keywords")
+        inp_sw.send_keys(sw_sch)
+        #####
+        #* Apertando o botão de "Search".
+        #? Seria bom tratar erros na busca? (Ex.: Periodo maior que 120 dias)
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "vuln-search-submit"))
+        )
+        element.click()
+        #####
+        #TODO: Verificar se a linha abaixo é necessaria.
+        url_n = driver.current_url
+        #####
+        #! A partir desse momento será utilizando muito mais o BeautifulSoup para tratar a HTML e o requests para acessar elas.
+        #* Usando o Requests para baixar a HTML e o Soup para "tratar" ela.
+        response = requests.get(url_n)
         content_t = response.content
         site_t = BeautifulSoup(content_t, 'html.parser')
-        #####
-        #* O for foi utilizado para localizar todos os <a> da HTML e a frente serão selecionados aqueles que possuem o termo "CVE"
-        for link in site_t.find_all('a'):
-            refadv = []
-            severity = []
-            affec =[]
-            src_lst = []
-            cpe_cd = []
-            publi_d = ''
-            sub_lst = []
-            if 'CVE' in str(link.contents):
-                #* Os links contidos na variavel localizada estavam incompletos, sendo necessario concatenar as str e gerar novo link.
-                new_link = "https://nvd.nist.gov" + str(link.get('href'))
-                driver.get(new_link)
-                cont_new = requests.get(new_link)
-                cont_new_t = cont_new.content
-                site_n_t = BeautifulSoup(cont_new_t, 'html.parser')
-                #print(site_n_t.prettify())
-                #####
-                #* Utilizando método find do BS para localizar o código CVE, armazenado na variavel cve_cd.
-                cve_cd = site_n_t.find(attrs = {'data-testid':'page-header-vuln-id'})
-                cve_cd = str(cve_cd.contents)[1:-1] #* Retirado caracteres a mais.
-                #####
-                #* Mesmo modelo utilizado anterior mente agora para loclaizar a descrição.
-                descrit = site_n_t.find('p', attrs={'data-testid': 'vuln-description'})
-                descrit = str(descrit.contents)[1:-1]
-                #####
-                #* Utilizando o método "find_all" do BS para localizar os graus de severidade anotados da vunerabilidade.
-                for busca in site_n_t.find_all(attrs={'id': re.compile('Cvss3')}):
-                    severity.append(str(busca.contents)[2:-2]) #? Verificar se é possivel identificar cada uma das notas
-                
-                #####
-                #* Mesmo principio porem agora para localizar links de esclarecimento do fornecedor do software
-                #* OBS.: Foi utilizado a biblioteca re junto com métodos compile para localizar de maneria geral palavras
-                #* que contenham a parte já especificada (inserido palavra parcial)
-                for busca in site_n_t.find_all(attrs= {'data-testid' : re.compile("vuln-hyperlinks-link-")}):
-                    refadv.append(busca.text)
-                
-                #####
-                #* Iniciando com try pois existem casos em que não é especificado o código "CPE" e quando ocorria o sw bugava.
-                try:
+        num_vuln = site_t.find(attrs={"data-testid": "vuln-matching-records-count"})
+        num_vuln = int(str(num_vuln.contents)[2:-2])
+        #print(type(num_vuln))
+        num_pg = []
+        lst_felps = []
+        if num_vuln > 20:
+            for busca in site_t.find_all(attrs={"aria-label": re.compile('Page')}):
+                num_pg.append("https://nvd.nist.gov"+str(busca["href"]))
+        else:
+            num_pg.append(url_n)
+        #print(num_pg)
+        for pg_b in num_pg:
+            response = requests.get(pg_b)
+            content_t = response.content
+            site_t = BeautifulSoup(content_t, 'html.parser')
+            #####
+            #* O for foi utilizado para localizar todos os <a> da HTML e a frente serão selecionados aqueles que possuem o termo "CVE"
+            for link in site_t.find_all('a'):
+                refadv = []
+                severity = []
+                affec =[]
+                src_lst = []
+                cpe_cd = []
+                publi_d = ''
+                sub_lst = []
+                if 'CVE' in str(link.contents):
+                    #* Os links contidos na variavel localizada estavam incompletos, sendo necessario concatenar as str e gerar novo link.
+                    new_link = "https://nvd.nist.gov" + str(link.get('href'))
+                    driver.get(new_link)
+                    cont_new = requests.get(new_link)
+                    cont_new_t = cont_new.content
+                    site_n_t = BeautifulSoup(cont_new_t, 'html.parser')
+                    #print(site_n_t.prettify())
                     #####
-                    #* Localizado a linha do HTML com json(s) inserido(s).
-                    for busca in site_n_t.find_all('input', attrs= {'id' : re.compile("cveTreeJsonDataHidden")}):
-                        tens = busca
+                    #* Utilizando método find do BS para localizar o código CVE, armazenado na variavel cve_cd.
+                    cve_cd = site_n_t.find(attrs = {'data-testid':'page-header-vuln-id'})
+                    cve_cd = str(cve_cd.contents)[1:-1] #* Retirado caracteres a mais.
+                    #####
+                    #* Mesmo modelo utilizado anterior mente agora para loclaizar a descrição.
+                    descrit = site_n_t.find('p', attrs={'data-testid': 'vuln-description'})
+                    descrit = str(descrit.contents)[1:-1]
+                    #####
+                    #* Utilizando o método "find_all" do BS para localizar os graus de severidade anotados da vunerabilidade.
+                    for busca in site_n_t.find_all(attrs={'id': re.compile('Cvss3')}):
+                        severity.append(str(busca.contents)[2:-2]) #? Verificar se é possivel identificar cada uma das notas
                     
                     #####
-                    #* Retirado o(s) json da linha do HTML e feito o split para casos em que há mais de um json.
-                    js_part = tens['value'][1:-1].split("[]}]}]},")
-                    #####
-                    #* Elaborado lista com os json corrigidos após o split.
-                    for ptr in range(0,len(js_part)):
-                        if ptr == len(js_part)-1:
-                            src_lst.append(js_part[ptr])
-                        else:
-                            src_lst.append(js_part[ptr]+"[]}]}]}")
+                    #* Mesmo principio porem agora para localizar links de esclarecimento do fornecedor do software
+                    #* OBS.: Foi utilizado a biblioteca re junto com métodos compile para localizar de maneria geral palavras
+                    #* que contenham a parte já especificada (inserido palavra parcial)
+                    for busca in site_n_t.find_all(attrs= {'data-testid' : re.compile("vuln-hyperlinks-link-")}):
+                        refadv.append(busca.text)
                     
                     #####
-                    #* Utilizado a biblioteca json + método loads para converter ele em um dicionario e ser tratado no codigo.
-                    for rg in range(0,len(src_lst)):
-                        scr = json.loads(src_lst[rg])
-                        cpe_cd.append(scr["containers"][0]["containers"][0]["cpes"][0]["cpe23Uri"]) #? Seria possivel melhorar?
-                        #? Esse levantamento foi feito com base no padrão da loclaização da informação, mas está muito "truncado"                    
-                except:
-                    cpe_cd.append("None")
-                #####
-                #* Mesma logica já utilizado para localizar conteudo com palavras parciais, agora para localizar a data de publicação.
-                for busca in site_n_t.find_all(attrs= {'data-testid' : re.compile("vuln-published-on")}):
-                    publi_d = str(busca.contents)
-                sub_lst.append(sw_sch)
-                sub_lst.append(cve_cd)
-                #print("Código CVE: ", cve_cd)
-                sub_lst.append(descrit)
-                #print("Descrição: ", descrit)
-                sub_lst.append(severity)
-                #print("Severidade: ", severity)
-                sub_lst.append(refadv)
-                #print("Referencia: ", refadv)
-                sub_lst.append(cpe_cd)
-                #print("Código CPE: ", cpe_cd)
-                sub_lst.append(publi_d)
-                #print("Data de publicação: ", publi_d)
-                sub_lst.append(new_link)
-                #print("Site da CVE", new_link)
-                #print(sub_lst)
-                lst_felps.append(sub_lst)
-                #time.sleep(1)
-                driver.back()
-        #####
-        ######
-    time.sleep(2)
-except:
-    driver.quit()
+                    #* Iniciando com try pois existem casos em que não é especificado o código "CPE" e quando ocorria o sw bugava.
+                    try:
+                        #####
+                        #* Localizado a linha do HTML com json(s) inserido(s).
+                        for busca in site_n_t.find_all('input', attrs= {'id' : re.compile("cveTreeJsonDataHidden")}):
+                            tens = busca
+                        
+                        #####
+                        #* Retirado o(s) json da linha do HTML e feito o split para casos em que há mais de um json.
+                        js_part = tens['value'][1:-1].split("[]}]}]},")
+                        #####
+                        #* Elaborado lista com os json corrigidos após o split.
+                        for ptr in range(0,len(js_part)):
+                            if ptr == len(js_part)-1:
+                                src_lst.append(js_part[ptr])
+                            else:
+                                src_lst.append(js_part[ptr]+"[]}]}]}")
+                        
+                        #####
+                        #* Utilizado a biblioteca json + método loads para converter ele em um dicionario e ser tratado no codigo.
+                        for rg in range(0,len(src_lst)):
+                            scr = json.loads(src_lst[rg])
+                            cpe_cd.append(scr["containers"][0]["containers"][0]["cpes"][0]["cpe23Uri"]) #? Seria possivel melhorar?
+                            #? Esse levantamento foi feito com base no padrão da loclaização da informação, mas está muito "truncado"                    
+                    except:
+                        cpe_cd.append("None")
+                    #####
+                    #* Mesma logica já utilizado para localizar conteudo com palavras parciais, agora para localizar a data de publicação.
+                    for busca in site_n_t.find_all(attrs= {'data-testid' : re.compile("vuln-published-on")}):
+                        publi_d = str(busca.contents)
+                    sub_lst.append(sw_sch)
+                    sub_lst.append(cve_cd)
+                    #print("Código CVE: ", cve_cd)
+                    sub_lst.append(descrit)
+                    #print("Descrição: ", descrit)
+                    sub_lst.append(severity)
+                    #print("Severidade: ", severity)
+                    sub_lst.append(refadv)
+                    #print("Referencia: ", refadv)
+                    sub_lst.append(cpe_cd)
+                    #print("Código CPE: ", cpe_cd)
+                    sub_lst.append(publi_d)
+                    #print("Data de publicação: ", publi_d)
+                    sub_lst.append(new_link)
+                    #print("Site da CVE", new_link)
+                    #print(sub_lst)
+                    lst_felps.append(sub_lst)
+                    #time.sleep(1)
+                    driver.back()
+            #####
+            ######
+        #time.sleep(2)
+    except:
+        driver.quit()
 
-driver.close()
+    driver.close()
 
-dados_monta_email = bot_registro_excel.montaPlanilha(lst_felps)
+    #dados_monta_email = bot_registro_excel.montaPlanilha(lst_felps)
+    return(lst_felps)
 
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import time
 import bot_web_scraping
+import bot_registro_excel
 import bot_envio_email
+
 
 print("-------------------------------------------------------------------------------------")
 print("-------------------------------------------------------------------------------------")
@@ -9,22 +12,102 @@ print("-------------------------------------------------------------------------
 print("-------------------------------------------------------------------------------------")
 print("-------------------------------------------------------------------------------------")
 ListaEmails = [
-    
+    'gelsinhomusico@gmail.com'
 ]
 #'gelsinhomusico@gmail.com',
 #    '2.gelsinhomusico@gmail.com',
 
-while True:
-    email_desejado = input("Primeiro digite o email para onde enviaremos os resultados: ")
+while True: # While do EMAIL
+    email_desejado = input("Digite o email para onde enviaremos os resultados: ")
     if ('@' not in email_desejado) or ('.' not in email_desejado):
         print('Email invalido, por gentileza digite novamente.')
         continue
     else:
+        ListaEmails.append(email_desejado)
+    resp = input("Deseja inserir outro email?[Y/N]")
+    print(resp)
+    while True:        
+        if resp.lower() == 'y' or resp.lower() == 'n':
+            break
+        else:
+            resp = input("Deseja inserir outro email?[Y/N]")
+            continue
+    if resp.lower() == 'y':
+        continue
+    else:
         break
-ListaEmails.append(email_desejado)
-ListaEmails = [num for num in reversed(ListaEmails)]
 
-bot_envio_email.send_email(ListaEmails, bot_web_scraping.dados_monta_email)
+
+while True: # While do Software
+    sw_sch = input("Insira o software que deseja pesquisar: ")#"log4J"
+    if sw_sch == '':
+        print('Dado invalido, digite novamente')
+        continue
+    else:
+        break    
+
+while True: #* Testador de datas SIMPLIFICADO, é necessario que as datas
+    while True:
+        data_busca = input("Insira a data de início do intervalo (formato mmddYY): ")#"01152022" #! A entrada de data deve estar no padrão americano mm dd YY
+        data_busca = data_busca.replace("/", "")
+        data_busca = data_busca.replace(" ", "")
+        if int(data_busca[0:2]) > 12:
+            print("Mes invalido")
+            continue
+        elif int(data_busca[2:4]) >= 32:
+            print("Dia invalido")
+            continue
+        elif (int(data_busca[0:2]) == 2) and (int(data_busca[2:4]) > 30):
+            print("Dia invalido")
+            continue
+        elif len(data_busca) != 6:
+            print("Formato de data invalido")
+            continue
+        break
+    while True:
+        aday = input("Insira a data de fim do intervalo (formato mmddYY): ")
+        aday = aday.replace("/", "")
+        aday = aday.replace(" ", "")
+        if int(aday[0:2]) > 12:
+            print("Mes invalido")
+            continue
+        elif int(aday[2:4]) >= 32:
+            print("Dia invalido")
+            continue
+        elif (int(aday[0:2]) == 2) and (int(aday[2:4]) > 30):
+            print("Dia invalido")
+            continue
+        elif len(aday) != 6:
+            print("Formato de data invalido")
+            continue
+        break
+
+    if int(data_busca[4:]) > int(aday[4:]):
+        print('Datas invalidas. 1')
+        continue
+    elif (int(data_busca[:2]) > int(aday[:2])):
+        print('Datas invalidas. 2')
+        continue
+    elif (int(data_busca[:2]) == int(aday[:2])) and (int(data_busca[2:4]) > int(aday[2:4])):
+        print('Datas invalidas. 3')
+        continue
+    else:
+        break
+
+print("-------------------------------------------------------------------------------------")
+time.sleep(2)
+print("OK! Não se assuste vamos lá! Jájá chega o email em...")
+print("-------------------------------------------------------------------------------------")
+time.sleep(5)
+
+#ListaEmails.append(email_desejado)
+#ListaEmails = [num for num in reversed(ListaEmails)]
+result_busca = bot_web_scraping.web_sc(sw_sch, data_busca, aday)
+
+felps_gel = bot_registro_excel.montaPlanilha(result_busca)
+
+bot_envio_email.send_email(ListaEmails, felps_gel)
+
 print("-------------------------------------------------------------------------------------")
 print("-------------------------------------------------------------------------------------")
 print("-------------------------------------------------------------------------------------")
